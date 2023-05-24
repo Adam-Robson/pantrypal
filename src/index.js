457bbe029cf5e082d1ea5266d00e3fc21093651e
Adam Robson <adamray312@gmail.com>
2023-05-24 13:53:03 -0700

    reboot, and start with bare minimum map

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { GoogleMapsProvider } from './context/GoogleContext';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <GoogleMapsProvider>
-      <App />
-    </GoogleMapsProvider>
-  </React.StrictMode>
-);
+ReactDOM
+  .createRoot(
+    document
+      .getElementById('root'))
+  .render(
+    <App />
+  );
