99590a69cb95152bbfa86df080314f3c26ce324b
George Ceja <net.george.sky@gmail.com>
2023-05-25 22:31:21 -0700

    remove extra api routes

--- backend/app.py
+++ backend/app.py
@@ -12,27 +12,11 @@ db = client.get_default_database()
 
 app = Flask(__name__)
 
-@app.route('/api/organizations/zipcode/<zipcode>')
-def fetch_orgs_by_zipcode(zipcode):
-    organizations = db.organizations.find(
-        {'zip_code': zipcode}, {'_id': False}
-    ).limit(5)
-
-    return dumps(list(organizations))
-
 @app.route('/api/organizations/city/<city_name>')
 def fetch_orgs_by_city(city_name):
     organizations = db.organizations.find(
         {'city': city_name}, {'_id': False}
-    ).limit(5)
-
-    return dumps(list(organizations))
-
-@app.route('/api/organizations/state/<state>')
-def fetch_orgs_by_state(state):
-    organizations = db.organizations.find(
-        {'state': state}, {'_id': False}
-    ).limit(5)
+    ).limit(20)
 
     return dumps(list(organizations))
 
