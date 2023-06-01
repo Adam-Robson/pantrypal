0dbd6f303760d0d9ea9326cdfe310c949a3d0e5c
Adam Robson <adamray312@gmail.com>
2023-05-31 17:45:00 -0700

    adding formatting to the modal and tutorial

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -7,6 +7,10 @@ export default function Modal({ onClose }) {
       <img src={ logo } alt="logo" />
       <h2 className="text-2xl font-semibold">Welcome!</h2>
       <p className="mx-8">Find free or affordable food options in your area so you can fully concentrate on your education.</p>
+      <div>
+        <a href="#" className="prenext previous round">&#8249;</a>
+        <a href="#" className="prenext next round">&#8250;</a>
+      </div>
       <button onClick={ onClose }>Skip tour</button>
     </section>
   );
