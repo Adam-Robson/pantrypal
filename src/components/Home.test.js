38cd74c602bd906cca2aeebe7452e7caf4037612
Adam Robson <adamray312@gmail.com>
2023-06-11 10:55:58 -0700

    changes to the Home tests to find tutorial link

--- src/components/Home.test.js
+++ src/components/Home.test.js
@@ -4,24 +4,13 @@ import Home from './Home';
 import { GoogleProvider } from '../context/GoogleContext';
 import { BrowserRouter } from 'react-router-dom';
 
-test('renders about nav link', () => {
+test('renders tutorial link', () => {
   render(
     <BrowserRouter>
       <GoogleProvider>
         <Home />
       </GoogleProvider>
     </BrowserRouter>);
-  const aboutNavLink = screen.getByText(/about/i);
-  expect(aboutNavLink).toBeInTheDocument();
-});
-
-test('renders about nav link', () => {
-  render(
-    <BrowserRouter>
-      <GoogleProvider>
-        <Home />
-      </GoogleProvider>
-    </BrowserRouter>);
-  const contactNavLink = screen.getByText(/contact/i);
-  expect(contactNavLink).toBeInTheDocument();
+  const tutorialLink = screen.getByText(/tutorial/i);
+  expect(tutorialLink).toBeInTheDocument();
 });
