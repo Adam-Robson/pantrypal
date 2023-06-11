a04df4d49383763bbb2e14133d78262ff0bc4565
GeorgeCloud <net.george.sky@gmail.com>
2023-06-10 21:54:53 -0700

    hotfix

---
+++ src/components/Markers.jsx
@@ -0,0 +1,19 @@
+import { Marker } from '@react-google-maps/api';
+import pin from '../assets/images/icons/orange-pin.svg';
+
+export default function Markers({ organizations, markerClick }) {
+  return (
+    <>
+      {
+        organizations.map((org, idx) => (
+          <Marker
+            key={ org.name }
+            position={ org.position }
+            options={{ icon: pin }}
+            onClick={() => markerClick(org, idx) }
+          />
+        ))
+      }
+    </>
+  );
+}
