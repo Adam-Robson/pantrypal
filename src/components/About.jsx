e9767ab393cd3dbc7a25e2dbb24550016a0a32dc
Adam Robson <adamray312@gmail.com>
2023-06-15 16:47:36 -0700

    create About page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,25 +3,37 @@ import { Link } from 'react-router-dom';
 import robert from '../assets/images/robert.png';
 import george from '../assets/images/george.png';
 import adam from '../assets/images/adam.png';
-
+import joel from '../assets/images/joel.png';
 export default function About() {
   return (
-    <section>
-      <Link className="menu-link relative right-96 top-10" to='/'>home</Link>
+    <section className="w-3/4 mx-auto">
+      <Link className="menu-link absolute top-6 left-8" to='/'>home</Link>
       <h1 className="text-2xl md:text-4xl my-20 subpixel-antialiased">About</h1>
       <article className="flex justify-around">
-        <Link to="">
-          <img className="h-40 rounded-full" src={ robert } alt="image of robert adkins" />
-        </Link>
-        <Link to="">
-          <img className="h-40 rounded-full" src={ george } alt="image of george ceja" />
-        </Link>
-        <Link to="">
-          <img className="h-40 rounded-full" src={ adam } alt="image of adam robson" />
-        </Link>
-        <Link to="">
-          <img className="h-40 rounded-full" src={ joel } alt="image of joel yap" />
-        </Link>
+        <div>
+          <Link to="">
+            <img className="h-60 rounded-full" src={ robert } alt="image of robert adkins" />
+          </Link>
+          <h2 className="subpixel-antialiased">Robert Adkins</h2>
+        </div>
+        <div>
+          <Link to="">
+            <img className="h-60 rounded-full" src={ george } alt="image of george ceja" />
+          </Link>
+          <h2>George Ceja</h2>
+        </div>
+        <div>
+          <Link to="">
+            <img className="h-60 rounded-full" src={ adam } alt="image of adam robson" />
+          </Link>
+          <h2>Adam Robson</h2>
+        </div>
+        <div>
+          <Link to="">
+            <img className="h-60 rounded-full" src={ joel } alt="image of joel yap" />
+          </Link>
+          <h2>Joel Yap</h2>
+        </div>
       </article>
     </section>
   );
