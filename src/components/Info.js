6130c930b89d057bcbe2f468f943468797603918
Adam Robson <adamray312@gmail.com>
2023-06-15 08:50:44 -0700

    salvage some of my earlier work for the floating window and tutorial

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 import { useGoogleContext } from '../context/GoogleContext';
 import useMapUtils from '../hooks/useMapUtils';
 
 export default function Info() {
   const { handleRoute, setFocus } = useMapUtils();
+
   const {
+    setIsDetailsPage,
     activeMarkerId,
     setActiveMarkerId,
     organizations,
@@ -39,50 +40,52 @@ export default function Info() {
   }
 
   return (
-    <article className="info min-w-max w-5/12 max-w-sm mx-auto h-1/4 rounded-md p-2 absolute left-0 right-0 bottom-10 mb-8">
-      <div className="w-full p-2 min-h-full">
-        <p className="flex items-end w-3/4 text-lg md:text-xl lg:text-2xl text-left font-semibold h-20">
-          {organization.name}
+    <section className="info max-w-xs md:max-w-md md:max-h-96 max-h-64 mx-auto rounded-lg absolute left-0 right-0 bottom-10">
+      <article className="min-h-fit min-w-full p-4">
+        <p className="w-full text-lg lg:text-2xl text-left font-semibold h-10 mb-4">
+          { organization.name }
         </p>
-        <p className="text-left text-xs sm:text-sm lg:text-lg mt-2">
-          {organization.address}
+        <p className="text-left text-sm sm:text-lg lg:text-xl">
+          { organization.address }
         </p>
-        <p className="flex text-left text-xs md:text-sm min-h-full">
-          {organization.city + ' ' + organization.state + ' ' + organization.zip_code}
+        <p className="text-left text-sm sm:text-lg lg:text-xl">
+          { organization.city + ' ' + organization.state + ' ' + organization.zip_code }
         </p>
-        <div className="flex justify-between items-baseline min-h-full">
-          <p className="text-left text-xs sm:text-sm lg:text-lg">
-            {organization.phone_num}
-          </p>
-          <Link className="details-link text-xs sm:text-sm lg:text-lg pr-4" to={`/${activeMarkerId}`}>
-            Details
-          </Link>
+        <div className="flex justify-between">
+          <p className="text-left text-sm sm:text-lg lg:text-xl mb-4">{ organization.phone_num }</p>
+          <button
+            className="details-btn text-sm sm:text-lg lg:text-xl"
+            onClick={ () => setIsDetailsPage(true) }
+          >Details</button>
         </div>
-
-        {organizations.length > 1 && (
+        
+        { organizations.length > 1 && (
           <>
             <div className="w-full flex justify-center items-baseline">
-              <div onClick={handlePreviousClick} className="mx-4 mt-2 text-xs sm:text-sm lg:text-lg hover:cursor-pointer">
+              <div onClick={ handlePreviousClick } className="mx-4 mt-2 text-xs sm:text-sm lg:text-lg hover:cursor-pointer">
                 <FiChevronLeft />
               </div>
               <button
                 className="float-btn px-8 py-2 m-2"
-                onClick={() => handleRoute(organization.address)}
+                onClick={ () => handleRoute(organization.address) }
               >
                 Directions
               </button>
               <button className="float-btn px-8 py-2 m-2">
-                <a href={'tel:' + organization.phone_num}>
+                <a href={ 'tel:' + organization.phone_num }>
                   Call
                 </a>
               </button>
-              <div onClick={handleNextClick} className="mx-4 mt-2 text-xs sm:text-sm lg:text-lg hover:cursor-pointer">
+              <div onClick={ handleNextClick } className="mx-4 mt-2 text-xs sm:text-sm lg:text-lg hover:cursor-pointer">
                 <FiChevronRight />
               </div>
             </div>
           </>
-        )}
-      </div>
-    </article>
+        ) }
+      </article>
+    </section>
   );
 }
+    
+       
+        
--- src/components/Tutorial.jsx
+++ src/components/Tutorial.jsx
@@ -1,22 +1,18 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import logo from '../assets/logo/stacked_logo.svg';
-import { FiX } from 'react-icons/fi';
+import { FiX, FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 import page1 from '../assets/images/icons/tut_1.png';
 import page2 from '../assets/images/icons/tut_2.png';
 import page3 from '../assets/images/icons/tut_3.png';
-
 export default function Tutorial() {
-
   let navigate = useNavigate();
-
   const [currentPage, setCurrentPage] = useState(0);
   const pages = [
     <Page1 key={ Page1 } />,
     <Page2 key={ Page2 } />,
     <Page3 key={ Page3 } />,
   ];
-
   function pageOne() {
     if (currentPage !== 0) {
       setCurrentPage(0);
@@ -32,7 +28,6 @@ export default function Tutorial() {
       setCurrentPage(2);
     }
   }
-
   return (
     <>
       <section className="tutorial h-screen flex flex-col justify-evenly">
@@ -46,18 +41,22 @@ export default function Tutorial() {
           <button className="tutorial-btn rounded-full p-2 md:p-4 mx-6 md:mx-12" onClick={ pageTwo }></button>
           <button className="tutorial-btn rounded-full p-2 md:p-4 mx-6 md:mx-12" onClick={ pageThree }></button>
         </div>
+
       </section>
     </>
   );
 }
-
 function Page1() {
   return (
     <div className="h-1/3">
+
       <img className="max-w-xs max-h-40 mx-auto" src={ page1 } alt="icon" />
 
       <h2 className="text-2xl text-black font-semibold mt-4 mb-8">Welcome!</h2>
       <p className="mx-20 text-black">Find free food options in your area so you can concentrate on your education.</p>
+      <div className="absolute right-0 bottom-96 mx-4">
+        <FiChevronRight />
+      </div>
     </div>
   );
 }
@@ -65,9 +64,15 @@ function Page1() {
 function Page2() {
   return (
     <div className="h-1/3">
+      <div className="absolute left-0 bottom-96 mx-4">
+        <FiChevronLeft />
+      </div>
       <img className="max-w-xs max-h-40 mx-auto" src={ page2 } alt="icon" />
       <h2 className="text-2xl text-black font-semibold mt-4 mb-8">First things First</h2>
       <p className="mx-20 text-black">To find food pantries in your local area, please enable location sharing services.</p>
+      <div className="absolute right-0 bottom-96 mx-4">
+        <FiChevronRight />
+      </div>
     </div>
   );
 }
@@ -75,10 +80,12 @@ function Page2() {
 function Page3() {
   return (
     <div className="h-1/3">
+      <div className="absolute left-0 bottom-96 mx-4">
+        <FiChevronLeft />
+      </div>
       <img className="max-w-xs max-h-40 mx-auto" src={ page3 } alt="icon" />
 
       <h2 className="text-2xl text-black font-semibold mt-4 mb-8">How it Works</h2>
-
       <p className="mx-20 text-black">Find your location, view pantry details, and choose the right one for your needs.</p>
     </div>
   );
