0c327b9e66aa79e02bb3a468bafc866099864ad6
Adam Robson <adamray312@gmail.com>
2023-06-15 15:02:46 -0700

    begin working on the about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import robert from '../assets/images/robert.png';
+import george from '../assets/images/george.png';
+import adam from '../assets/images/adam.png';
 
 export default function About() {
   return (
-    <div>
+    <section>
       <Link className="menu-link relative right-96 top-10" to='/'>home</Link>
       <h1 className="text-2xl md:text-4xl my-20 subpixel-antialiased">About</h1>
-    </div>
+      <article className="flex justify-around">
+        <Link to="">
+          <img className="h-40 rounded-full" src={ robert } alt="image of robert adkins" />
+        </Link>
+        <Link to="">
+          <img className="h-40 rounded-full" src={ george } alt="image of george ceja" />
+        </Link>
+        <Link to="">
+          <img className="h-40 rounded-full" src={ adam } alt="image of adam robson" />
+        </Link>
+        <Link>
+      Joel Yap
+        </Link>
+      </article>
+    </section>
   );
 }
