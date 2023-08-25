b81f270ec997b222753e904f059f79fa8073a0e1
Adam Robson <adamray312@gmail.com>
2023-08-25 22:08:49 +0000

    Aug 25, 2023, 3:08 PM

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,9 @@
 export default {
     'env': {
         'browser': true,
-        'es2021': true
+        'es2021': true,
+        "jest": true,
+        "node": true
     },
     'extends': [
         'eslint:recommended',
@@ -22,7 +24,7 @@ export default {
     'rules': {
         'indent': [
             'error',
-            4
+            2
         ],
         'linebreak-style': [
             'error',
@@ -35,6 +37,47 @@ export default {
         'semi': [
             'error',
             'always'
-        ]
-    }
+        ],
+        'no-console': ['warn', {'allow': ['info', 'error']}],
+        'eqeqeq': ['error', 'always'],
+        'no-trailing-spaces': 'error',
+        'arrow-spacing': ['error', {'before': true, 'after': true}],
+        'react-hooks/exhaustive-deps': 1,
+        'react-hooks/rules-of-hooks': 2,
+        'react/jsx-uses-react': 'off',
+        'react/jsx-uses-vars': 'off',
+
+        'no-undef': 'warn',
+        'no-case-declarations': 'off',
+        'no-empty': 'off',
+        "block-spacing": "error",
+        "brace-style": "error",
+        "computed-property-spacing": "error",
+        "func-call-spacing": "error",
+        "key-spacing": "error",
+
+        "no-whitespace-before-property": "error",
+        "semi-spacing": "error",
+        "space-before-blocks": "error",
+
+
+        'no-redeclare': [ 'error', { 'builtInGlobals': true }],
+        'semi': ['error', 'always'],
+        'space-in-parens': 'error',
+        'space-infix-ops': 'error',
+        'object-curly-spacing': ['error', 'always'],
+        'comma-spacing':'error',
+        'space-before-function-paren': ['error', { 'asyncArrow': true, 'anonymous': false, 'named': false }],
+        'keyword-spacing': ['error', { 'before': 'error', 'after': 'error' }],
+        'array-bracket-spacing': 'error',
+        'react/prop-types': 0,
+        'no-unused-vars': 'warn',
+        'react/react-in-jsx-scope': 0,
+
+
+
+
+        "no-multiple-empty-lines": "error",
+        "no-trailing-spaces": "error",
+  }
 };
