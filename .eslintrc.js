c94764706936ec92a6093e557b08d0665272ca2d
Adam Robson <adamray312@gmail.com>
2023-08-25 21:51:15 +0000

    Aug 25, 2023, 2:51 PM

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,16 @@
-module.exports = {
+export default {
     'env': {
         'browser': true,
         'es2021': true
     },
     'extends': [
         'eslint:recommended',
-        'plugin:react/recommended'
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended'
     ],
     'parserOptions': {
         'ecmaFeatures': {
+            'implied strict': true,
             'jsx': true
         },
         'ecmaVersion': 12,
